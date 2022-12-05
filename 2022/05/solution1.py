8dce91323b3f6db489fadb42ccbefc70694a4f74
Mattias Brändström <svala@spotify.com>
2022-12-05 10:33:49 +0100

    2022-12-05 Python

---
+++ 2022/05/solution1.py
@@ -0,0 +1,56 @@
+import re
+import sys
+from queue import LifoQueue
+
+
+def get_stacks():
+    parsed_items = {}
+    n = 0
+    while True:
+        l = sys.stdin.readline()
+        if not '[' in l:
+            break
+        for i, j in enumerate(range(1, len(l), 4)):
+            n = max(n, i)
+            c = l[j]
+            if c != ' ':
+                items = parsed_items.get(i, LifoQueue())
+                items.put(c)
+                parsed_items[i] = items
+
+    stacks = []
+    for i in range(n + 1):
+        items = parsed_items[i]
+        stack = []
+        while not items.empty():
+            stack.append(items.get_nowait())
+        stacks.append(stack)
+    sys.stdin.readline()
+    return stacks
+
+
+def get_moves():
+    moves = []
+    for l in sys.stdin.readlines():
+        m = re.match(r'move (\d+) from (\d+) to (\d+)', l)
+        if m:
+            moves.append((int(m.group(1)), int(m.group(2)) - 1, int(m.group(3)) - 1))
+        else:
+            raise RuntimeError('no match!')
+    return moves
+
+
+def main():
+    stacks = get_stacks()
+    moves = get_moves()
+
+    for m in moves:
+        for _ in range(m[0]):
+            item = stacks[m[1]].pop()
+            stacks[m[2]].append(item)
+
+    print(''.join(stack.pop() for stack in stacks))
+
+
+if __name__ == '__main__':
+    main()
