fe90bace3746e584a7eebfcd917e486559bb9bd2
Mattias Brändström <svala@spotify.com>
2022-12-07 10:49:27 +0100

    2022-12-07 Python

---
+++ 2022/07/solution1.py
@@ -0,0 +1,63 @@
+import sys
+
+
+def read_file(l):
+    size, name = l.split(' ')
+    return int(size), name
+
+
+def split(input):
+    path = []
+    p = 0
+    while True:
+        if p >= len(input):
+            break
+
+        l = input[p]
+        p += 1
+        if l == '$ cd ..':
+            path.pop()
+        elif l.startswith('$ cd'):
+            _, _, dir_name = l.split(' ')
+            if dir_name == '/':
+                path = []
+            else:
+                path.append(dir_name)
+        else:
+            assert(l == '$ ls')
+            contents = []
+            while True and p < len(input):
+                l = input[p]
+                if input[p].startswith('$'):
+                    break
+                if l.startswith('dir'):
+                    _, inner_dir_name = l.split(' ')
+                    contents.append(('d', '/'.join(path + [inner_dir_name])))
+                else:
+                    size, file_name = read_file(l)
+                    contents.append(('f', file_name, size))
+                p += 1
+            yield '/'.join(path), contents
+
+
+def dir_size(d, dirs):
+    t = 0
+    for e in d:
+        if e[0] == 'f':
+            t += e[2]
+        else:
+            t += dir_size(dirs[e[1]], dirs)
+    return t
+
+
+def main():
+    input = [l.strip() for l in sys.stdin.readlines()]
+    dirs = dict(split(input))
+
+    print(sum(size for size in
+              (dir_size(d, dirs) for d in dirs.values())
+              if size <= 100000))
+
+
+if __name__ == '__main__':
+    main()
